f78698fa3857fdf622fc86ff970c39744bfd990b
Xande974 <alexandrenogueira974@gmail.com>
2021-02-25 14:06:54 -0400

    First commit

---
+++ BotDoXande/settings/config.js
@@ -0,0 +1,6 @@
+const config = {
+        botName: 'BotDoXande',
+        ownerName: 'Xande',
+        youtube: https://youtube.com/c/XANDEFFVenhaFazerParteDaNossaFamilia
+        instagram: https://instagram.com/on.xande_ff?igshid=dh3syym72vqq
+}
---
+++ BotDoXande/src/toinmenu.js
@@ -0,0 +1,12 @@
+const xandemenu = (prefix, pushname) => {
+    return `◪ *Comandos do Xande*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.xandemenu = xandemenu
